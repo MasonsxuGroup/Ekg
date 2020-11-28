6f5ebf8ed703729c2591bdb0a1e8327cd9ad24cf
masonsxu <masonsxu@foxmail.com>
2020-11-28 14:18:00 +0800

    Update 分工标注数据合并代码

--- Data_Pre/merge_all_data.py
+++ Data_Pre/merge_all_data.py
@@ -9,27 +9,29 @@ def get_data(folderpath_origin):
     print('开始合并所有文件中的数据')
     for name_index in trange(len(filename)):
         name = filename[name_index]
-        if len(name) == 22:
+        if len(name) == 13:
             path = folderpath_origin + name
             with open(path, 'r') as f:
                 for chunk in f:
-                    merged_data_list.append(chunk)
+                    if str(chunk)[0] != ' ':
+                        merged_data_list.append(chunk)
         else:
             print('\n不是目标文件，跳过当前文件。')
     return merged_data_list
 
 
 def save_merged_data(all_pre_data, folderpath_dest):
-    all_data = []
     print('开始保存所有数据')
-    for data_index in trange(len(all_pre_data)):
-        data = str(all_pre_data[data_index]).replace(
-            ',', ' ').replace('\n', '')
-        all_data.append(data)
-    print(all_data[0:10])
-    sheet = pd.DataFrame(all_data)
     filename = folderpath_dest + 'all_marked_data.txt'
-    sheet.to_csv(filename, index=None, header=None)
+    with open(filename, 'w') as f:
+        for data_index in trange(len(all_pre_data)):
+            data = str(all_pre_data[data_index]).replace(
+                ',', ' ').replace('\n', '')
+            if '。' in list(data):
+                f.write(data + '\n')
+                f.write('\n')
+            elif (len(data) > 1) and (str(data)[0] != ' '):
+                f.write(data + '\n')
 
 
 def run(folderpath_origin, folderpath_dest):
