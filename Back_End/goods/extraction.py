fa22f346d959b74fd9b76f01e93b1ffa20effc45
masonsxu <masonsxu@foxmail.com>
2020-12-08 16:20:05 +0800

    实体抽取修改json数据顺序错乱、关系图输出重复

--- Back_End/goods/extraction.py
+++ Back_End/goods/extraction.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from LAC import LAC
 
@@ -17,15 +18,15 @@ class ExtractData:
         my_lac.load_customization('Model_Code/data/dict.txt', sep=None)
         result_list = my_lac.run(text)
         result_data_dict = {}
-        result_data_dict['data'] = []
+        result_data_dict["data"] = []
         for index in range(len(result_list[0])):
             result_dict = {}
-            result_dict['item'] = result_list[0][index]
-            result_dict['pos'] = result_list[1][index]
-            result_data_dict['data'].append(result_dict)
-        sheet = pd.DataFrame(result_data_dict)
+            result_dict["item"] = result_list[0][index]
+            result_dict["pos"] = result_list[1][index]
+            result_data_dict["data"].append(result_dict)
         file_path = 'Back_End/static/data/result_data_dict.json'
-        sheet.to_json(file_path)
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(str(result_data_dict).replace("'", '"'))
         return result_data_dict
 
     def load_figure_data():
@@ -55,9 +56,9 @@ class ExtractData:
                     result_dict["TIME"] = []
                     result_dict["RES"] = []
                 result_dict["LOC"] = row["item"]
-            elif row["pos"] == 'TIME':
+            elif row["pos"] == 'TIME' and row["item"] not in result_dict["TIME"]:
                 result_dict["TIME"].append(row["item"])
-            elif row["pos"] == 'RES':
+            elif row["pos"] == 'RES' and row["item"] not in result_dict["RES"]:
                 result_dict["RES"].append(row["item"])
             if index == (len(result_data_dict['data'])-1):
                 figure_data_dict['data'].append(result_dict)
