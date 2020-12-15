697f8e99a0195c6a70e8a2a69fe9cc23568d0fce
masonsxu <masonsxu@foxmail.com>
2020-12-15 11:50:29 +0800

    提升性能由selenium获取cookie，request获取页面

--- Crawler_Job_Ekg/crawler_ekg.py
+++ Crawler_Job_Ekg/crawler_ekg.py
@@ -1,67 +1,106 @@
-import re
 import time
+import string
+import requests
 import pandas as pd
 from tqdm.std import trange
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 from multiprocessing import cpu_count
 from multiprocessing.pool import Pool
 from selenium.webdriver import Firefox
 from selenium.webdriver import FirefoxOptions
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-desired_capabilities = DesiredCapabilities.CHROME
-desired_capabilities['pageLoadStrategy'] = 'none'
-crawler_service = Service(executable_path='geckodriver',)
-crawler_service.command_line_args()
-crawler_service.start()
-option = FirefoxOptions()
-option.add_argument("--headless")  # 隐藏浏览器
-browser = Firefox(options=option)
+
+def find_href(href_list):
+    return len(href_list) == 3
+
+
+def find_news_data(news_data_list):
+    return len(news_data_list) == 4
+
+
+def get_html(url, cookie=None):
+    ua = UserAgent(verify_ssl=False)  # 调用UserAgent库生成ua对象
+    header = {'User-Agent': ua.random, "Cookie": cookie}
+    try:
+        r = requests.get(url, timeout=300, headers=header)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding  # 指定编码形式
+        return r.text
+    except:
+        return "please inspect your url or setup"
+
+
+def get_cookie(url):
+    option = FirefoxOptions()
+    option.set_preference('permissions.default.image', 2)  # 设置无图模式
+    option.add_argument('--headless')  # 设置无头模式
+    browser = Firefox(options=option)
+    browser.get(url)
+    time.sleep(5)
+    Cookies = browser.get_cookies()
+    browser.quit()
+    cookies = ''
+    # 获取cookie中的name和value,转化成requests可以使用的形式
+    for cookie in Cookies:
+        cookies += cookie['name'] + "=" + cookie['value'] + ";"
+    return cookies
 
 
 def get_title_url(url):
+    global cookie
     href_lists = []
-    browser.get(url)
-    time.sleep(3)
-    html = browser.page_source
-    soup = BeautifulSoup(html, 'lxml')
     try:
+        html = get_html(url, cookie=cookie)
+        soup = BeautifulSoup(html, 'lxml')
         uls = soup.find('ul', {'class': 'zxxx_list'})
         for li in uls.find_all('li'):
             a_list = []
             a_tag = li.find('a')
             a_list.append(a_tag['title'])
-            a_list.append('http://www.nhc.gov.cn' + a_tag['href'])
+            if (href := a_tag['href'])[:4] != "http":
+                a_list.append('http://www.nhc.gov.cn' + href)
             a_list.append(li.find('span').text)
             href_lists.append(a_list)
     except Exception as e:
-        print(e)
+        print("======>>>ERROR: 重新获取 Cookie <<<======")
+        cookie = get_cookie(url)
+        html = get_html(url, cookie=cookie)
+        soup = BeautifulSoup(html, 'lxml')
+        uls = soup.find('ul', {'class': 'zxxx_list'})
+        for li in uls.find_all('li'):
+            a_list = []
+            a_tag = li.find('a')
+            a_list.append(a_tag['title'])
+            if (href := a_tag['href'])[:4] != "http":
+                a_list.append('http://www.nhc.gov.cn' + href)
+            a_list.append(li.find('span').text)
+            href_lists.append(a_list)
     return href_lists
 
 
 def get_news_data(title_ulr_list):
+    global cookie
     news_list = []
-    browser.get(title_ulr_list[1])
-    time.sleep(3)
-    html = browser.page_source
-    soup = BeautifulSoup(html, 'lxml')
+    news_list.append(title_ulr_list[0])
+    news_list.append(title_ulr_list[1])
+    news_list.append(title_ulr_list[2])
     try:
-        news_list.append(title_ulr_list[0])
-        news_list.append(title_ulr_list[1])
-        news_list.append(title_ulr_list[2])
+        html = get_html(title_ulr_list[1], cookie=cookie)
+        soup = BeautifulSoup(html, 'lxml')
         content = soup.find('div', {'class': 'con'}).text
-        punc = "\n\t "  # 需要删除的特殊字符
+        punc = string.whitespace  # 需要删除的特殊字符
         trans = str.maketrans({key: None for key in punc})  # 删除特殊字符
-        news_list.append(
-            str(content)
-            .translate(trans)
-            .replace('\u2002', '')
-            .replace('\xa0', '')
-            .replace('分享到', '')
-        )
+        news_list.append(str(content).translate(trans).replace('分享到', ''))
     except Exception as e:
-        print(e)
+        print("======>>>ERROR: 重新获取 Cookie <<<======")
+        cookie = get_cookie(title_ulr_list[1])
+        html = get_html(title_ulr_list[1], cookie=cookie)
+        soup = BeautifulSoup(html, 'lxml')
+        content = soup.find('div', {'class': 'con'}).text
+        punc = string.whitespace  # 需要删除的特殊字符
+        trans = str.maketrans({key: None for key in punc})  # 删除特殊字符
+        news_list.append(str(content).translate(trans).replace('分享到', ''))
     return news_list
 
 
@@ -75,7 +114,7 @@ def sand_url():
             + "/list_gzbd.shtml"
         )
         urls.append(url)
-        for page in range(2, int(section_type['pagination'])):
+        for page in range(2, int(section_type['pagination']) + 1):
             url = (
                 "http://www.nhc.gov.cn/xcs/"
                 + str(section_type['type'])
@@ -115,9 +154,15 @@ def sort_out_news(func, title_ulr_lists):
     return news_data_lists
 
 
+def save_titles_data(folderpath_dest, title_ulr_lists):
+    sheet = pd.DataFrame(title_ulr_lists, columns=["标题", "url", "日期"])
+    filename = folderpath_dest + "/" + "新冠肺炎相关事件（标题）.csv"
+    sheet.to_csv(filename, index=None)
+
+
 def save_news_data(folderpath_dest, news_data_lists):
     sheet = pd.DataFrame(news_data_lists, columns=["标题", "url", "日期", "详细"])
-    filename = folderpath_dest + "/" + "新冠肺炎相关事件.csv"
+    filename = folderpath_dest + "/" + "新冠肺炎相关事件（详细）.csv"
     sheet.to_csv(filename, index=None)
 
 
@@ -125,16 +170,16 @@ def run(folderpath_dest):
     try:
         urls = sand_url()
         title_ulr_lists = sort_out_href(get_title_url, urls)
+        title_ulr_lists = list(filter(find_href, title_ulr_lists))
+        save_titles_data(folderpath_dest, title_ulr_lists)
         news_data_lists = sort_out_news(get_news_data, title_ulr_lists)
+        news_data_lists = list(filter(find_news_data, news_data_lists))
         save_news_data(folderpath_dest, news_data_lists)
-        browser.quit()
-        crawler_service.stop()
     except Exception as e:
         print(e)
-        browser.quit()
-        crawler_service.stop()
 
 
+cookie = get_cookie("http://www.nhc.gov.cn/xcs/yqtb/list_gzbd.shtml")
 if __name__ == '__main__':
     folderpath_dest = 'Crawler_Job_Ekg/NHC_Data'
     run(folderpath_dest)
