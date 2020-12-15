096a5bb48e151d3f7330ba94c2542bfb3de46ea4
masonsxu <masonsxu@foxmail.com>
2020-12-15 18:52:50 +0800

    修改代码排版

--- Data_Pre/merge_all_data.py
+++ Data_Pre/merge_all_data.py
@@ -9,6 +9,7 @@ def get_data(folderpath_origin):
     Args:
         :folderpath_origin (str): Folder，待加载数据文件目录
     """
+
     filename = os.listdir(folderpath_origin)
     merged_data_list = []
     print('开始合并所有文件中的数据')
@@ -32,12 +33,12 @@ def save_merged_data(all_pre_data, folderpath_dest):
         :all_pre_data (list): list of pre data，合并后的数据列表
         :folderpath_dest (str): path of folder，处理好的数据保存目录
     """
+
     print('开始保存所有数据')
     filename = folderpath_dest + 'all_marked_data.txt'
     with open(filename, 'w') as f:
         for data_index in trange(len(all_pre_data)):
-            data = str(all_pre_data[data_index]).replace(
-                ',', ' ').replace('\n', '')
+            data = str(all_pre_data[data_index]).replace(',', ' ').replace('\n', '')
             if '。' in list(data):
                 f.write(data + '\n')
                 f.write('\n')
